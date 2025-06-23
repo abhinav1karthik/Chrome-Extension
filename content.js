817429d2061d2784df19bad3a7765f93e4c4b317
Abhinav Karthik <abhinavkarthik.pattipati@gmail.com>
2025-06-23 23:17:44 +0530

    add button on webpage

--- content.js
+++ content.js
@@ -1,7 +1,21 @@
-// content.js
-// Author:
-// Author URI: https://
-// Author Github URI: https://www.github.com/
-// Project Repository URI: https://github.com/
-// Description: Handles all the webpage level activities (e.g. manipulating page data, etc.)
-// License: MIT
+const bookmarkImgURL = chrome.runtime.getURL("assets/bookmark.png");
+
+window.addEventListener("load", addBookmarkButton);
+
+function addBookmarkButton() {
+  const bookmarkButton = document.createElement("img");
+  bookmarkButton.id = "add-bookmark-button";
+  bookmarkButton.src = bookmarkImgURL;
+  bookmarkButton.style.height = "30px";
+  bookmarkButton.style.width = "30px";
+  bookmarkButton.style.cursor = "pointer";
+  bookmarkButton.alt = "Bookmark Problem";
+  const container = document.querySelector(
+    "div.ant-row.d-flex.gap-4.mt-1.css-19gw05y"
+  );
+  if (container) {
+    container.appendChild(bookmarkButton);
+  } else {
+    console.warn("Bookmark container not found");
+  }
+}
